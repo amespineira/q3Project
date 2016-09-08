4664af3e113667f704f5e4d01236898d5a805cc3
amespineira <amespineira@me.com>
2016-09-08 14:11:49 -0600

    same

--- src/main/java/q3Project/main/App.java
+++ src/main/java/q3Project/main/App.java
@@ -37,7 +37,7 @@ public class App {
    // JDBC driver name and database URL
 	static final String JDBC_DRIVER = "org.postgresql.Driver";
 //	static final String DB_URL = "jdbc:postgresql://localhost/testdb";
-	static final String DB_URL = System.getenv("JDBC_DATABASE_URL");
+	static final String DB_URL = System.getenv("DATABASE_URL");
     interface Validable {
         boolean isValid();
     }
